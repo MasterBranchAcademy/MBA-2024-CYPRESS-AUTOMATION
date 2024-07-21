0c1099ae50ce0de192987d08e41bc0595e622f68
ademyavuz42 <ademyavuz@gmail.com>
2024-07-21 19:32:00 +0700

    scrum 54 and 67

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -13,6 +13,12 @@ const LOCATORS = {
     promotionItem: '.col-md-3.col-xs-6.promo_block',
     promotionDescription: '.promo_text',
     promotionImage: '.promo_icon',
+    logo: '.logo',
+    mainMenu: '.subnav',  
+    footer: 'footer',
+    eachCategory: '.nav-pills.categorymenu > li > a',
+    pathImage: '.parent_cat_image',
+    catImage: '.root_cat_image', 
   },
   LOGIN_PAGE: {
   },
